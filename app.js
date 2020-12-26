a9686e4e0a181223c048959eb63f3a412226eadf
sarahTarablus <sarahtarablus@gmail.com>
2020-12-26 11:03:42 -0800

    numbers added

---
+++ app.js
@@ -0,0 +1,69 @@
+const btn1 = document.getElementById('btn-1');
+const btn2 = document.getElementById('btn-2');
+const btn3 = document.getElementById('btn-3');
+const btn4 = document.getElementById('btn-4');
+const btn5 = document.getElementById('btn-5');
+const btn6 = document.getElementById('btn-6');
+const btn7 = document.getElementById('btn-7');
+const btn8 = document.getElementById('btn-8');
+const btn9 = document.getElementById('btn-9');
+const btn0 = document.getElementById('btn-0');
+const btnPoint = document.getElementById('btn-point');
+const btnEqual = document.getElementById('btn-equal');
+const btnMul = document.getElementById('btn-mul');
+const btnDiv = document.getElementById('btn-div');
+const btnPlus = document.getElementById('btn-plus');
+const btnMin = document.getElementById('btn-min');
+const btnCE = document.getElementById('btn-ce');
+const display = document.getElementById('display');
+
+btn1.addEventListener('click', function(){displayNum(1)});
+btn2.addEventListener('click', function(){displayNum(2)});
+btn3.addEventListener('click', function(){displayNum(3)});
+btn4.addEventListener('click', function(){displayNum(4)});
+btn5.addEventListener('click', function(){displayNum(5)});
+btn6.addEventListener('click', function(){displayNum(6)});
+btn7.addEventListener('click', function(){displayNum(7)});
+btn8.addEventListener('click', function(){displayNum(8)});
+btn9.addEventListener('click', function(){displayNum(9)});
+btn0.addEventListener('click', function(){displayNum(0)});
+btnPoint.addEventListener('click', function(){addPoint('.')});
+btnCE.addEventListener('click', clearDisplay);
+btnPlus.addEventListener('click', addNumbers);
+
+let displayNumber = '';
+
+function displayNum(num){
+  if(displayNumber.length < 15){
+    displayNumber += num;
+    display.innerHTML = displayNumber;
+    console.log(displayNumber);
+  }
+}
+
+//make sure only two points consecutive can not be written
+function addPoint(point){
+  displayNumber += point;
+  display.innerHTML = displayNumber;
+  console.log(displayNumber);
+} 
+
+
+function clearDisplay(){
+  if( display.innerHTML !== ''){
+    display.innerHTML = '';
+    displayNumber = '';
+    display.innerHTML = '0';
+   }
+}
+
+function addNumbers(){
+  let x = displayNumber;
+  if( display.innerHTML !== ''){
+    display.innerHTML = '';
+    displayNumber = '';
+   }
+}
+
+
+
